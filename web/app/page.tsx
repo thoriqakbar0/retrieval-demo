24da033fb377519be0c43954128bcbce0676d097
thoriqakbar0 <41317726+thoriqakbar0@users.noreply.github.com>
2025-01-01 16:40:44 +0700

    feat: Add API fetch and display message in Home component

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,12 @@
 import { Chat } from "@/components/chat";
 
-export default function Home() {
+export default async function Home() {
+  const hello = await fetch("http://localhost:8000/api/hello")
+  const data = await hello.json()
+  console.log(data)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] debug">
-      Comparing different retrieval strategies
+      Comparing different retrieval strategies: {data.message}
       <Chat />
     </div>
   );
